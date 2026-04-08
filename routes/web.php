93e84d86578bcc7a88cccc4a934b52747212e91b
andinifantuzzia <fantuzziaa@gmail.com>
2026-04-08 10:42:45 +0700

    Tambah tampilan homepage

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,19 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\LoginController;
+use app\http\controllers\homepage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-
+Route::get('/homepage', function () {
+    return view('homepage');
+});
 
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::get('/admin/dashboard', [AdminController::class, 'index']);
+
+
